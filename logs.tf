41c90739fbb2cd71772f30156560300daed6d030
Vinod Sai Kumar <60657267+vinod-global@users.noreply.github.com>
2021-01-13 15:25:11 +0000

    fix: updated the module to latest version (#3)

--- logs.tf
+++ logs.tf
@@ -1,5 +1,5 @@
 module "logs" {
-  source = "github.com/global-devops-terraform/k8s-logs?ref=v0.23.1"
+  source = "github.com/global-devops-terraform/k8s-logs?ref=v0.27.0"
 
   read_access_groups = {
     "devaccess" = ["devaccess"]
