c6f387a7dc8bf4f1899108e0fb6f0fc7426acab7
Vinod Sai Kumar <vinodsai.kumar@global.com>
2021-01-13 15:01:06 +0000

    fix: updated the module to latest version

--- terraform.tf
+++ terraform.tf
@@ -3,6 +3,29 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = local.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = local.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
 terraform {
   backend "s3" {
     bucket         = "global-self-service-dev-terraform-state"
